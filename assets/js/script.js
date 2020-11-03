10a758776d3f65c13c146d79831d7b986dfc2943
jbeedle19 <josh.beedle@gmail.com>
2020-11-03 17:19:04 -0500

    refactored code for form and creating a new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do")
 
-var createTaskHandler = function() {
+var taskFormHandler = function(event) {
 
     // Prevents the page from reloading when form is submitted
     event.preventDefault();
@@ -11,6 +11,17 @@ var createTaskHandler = function() {
     // Creates a variable that will be what is selected as the task type
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // Package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // Send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
     // a new <li> element is created
     var listItemEl = document.createElement("li");
     // it is given the "task-item" class to be styled
@@ -21,17 +32,17 @@ var createTaskHandler = function() {
     // Give it a class name
     taskInfoEl.className = "task-info";
     // Add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span";
 
     // Add it to the HTML
     listItemEl.appendChild(taskInfoEl);
     
     // and finally it will be added into the HTML
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
-// When the User submits the form to add task, createTaskHandler runs
+// When the User submits the form to add task, taskFormHandler runs
 // It is listening for the User to hit submit OR hit the enter key
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 
