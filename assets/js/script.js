dcb945e328a416f871f35971481f84d70d28bb89
jbeedle19 <josh.beedle@gmail.com>
2020-11-02 23:56:56 -0500

    Updated form so that it submits the task name and task type

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,26 @@ var createTaskHandler = function() {
     // Prevents the page from reloading when form is submitted
     event.preventDefault();
 
+    // Creates a variable that will be what is typed in the task name field
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // Creates a variable that will be what is selected as the task type
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     // a new <li> element is created
     var listItemEl = document.createElement("li");
     // it is given the "task-item" class to be styled
     listItemEl.className = "task-item";
-    // the content added will say "This is a new task"
-    listItemEl.textContent = "This is a new task.";
+
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // Give it a class name
+    taskInfoEl.className = "task-info";
+    // Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span";
+
+    // Add it to the HTML
+    listItemEl.appendChild(taskInfoEl);
+    
     // and finally it will be added into the HTML
     tasksToDoEl.appendChild(listItemEl);
 };
